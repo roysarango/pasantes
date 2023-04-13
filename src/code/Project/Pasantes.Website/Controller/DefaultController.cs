c4c174cae2a985924295108b0e041d3806fb1d20
roy.sarango <rsa@alpha-solutions.us>
2023-04-13 14:32:45 -0500

    Add layout and first component

---
+++ src/code/Project/Pasantes.Website/Controller/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Pasantes.Website.Controller
+{
+    public class DefaultController : System.Web.Mvc.Controller
+    {
+        public ActionResult Sample()
+        {
+            return View("~/Views/Sample.cshtml");
+        }
+    }
+}
