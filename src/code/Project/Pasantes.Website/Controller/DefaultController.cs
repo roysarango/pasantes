b129df1d7709efc24bf79389a44cb467a4cdbcab
roy.sarango <rsa@alpha-solutions.us>
2023-04-17 14:02:46 -0500

    Add TitleAndText component

--- src/code/Project/Pasantes.Website/Controller/DefaultController.cs
+++ src/code/Project/Pasantes.Website/Controller/DefaultController.cs
@@ -1,12 +1,38 @@
-﻿using System.Web.Mvc;
+﻿using Sitecore.Mvc.Presentation;
+using System.Web.Mvc;
+using Pasantes.Website.Models;
+using Pasantes.Website.Services.GlassMapper;
 
 namespace Pasantes.Website.Controller
 {
     public class DefaultController : System.Web.Mvc.Controller
     {
+        private readonly IRenderingService _renderingService;
+
+        public DefaultController(IRenderingService renderingService)
+        {
+            _renderingService = renderingService;
+        }
+
         public ActionResult Sample()
         {
             return View("~/Views/Sample.cshtml");
+        }
+
+        public ActionResult TitleAndText()
+        {
+            var dataSourceId = RenderingContext.CurrentOrNull.Rendering.DataSource;
+
+            var item = Sitecore.Context.Database.GetItem(dataSourceId);
+
+            return View("~/Views/TitleAndText.cshtml", item);
+        }
+
+        public ActionResult TitleAndTextGlassMapper()
+        {
+            var item = _renderingService.GetDataSource<TitleAndText>();
+
+            return View("~/Views/TitleAndTextGlassMapper.cshtml", item);
         }
     }
 }
---
+++ src/code/Project/Pasantes.Website/Models/TitleAndText.cs
@@ -0,0 +1,14 @@
+﻿using Glass.Mapper.Sc.Fields;
+using Pasantes.Website.Models.GlassMapper;
+
+namespace Pasantes.Website.Models
+{
+    public class TitleAndText : BaseItem
+    {
+        public virtual string Title { get; set; }
+
+        public virtual string Text { get; set; }
+
+        public virtual Image Image1 { get; set; }
+    }
+}
