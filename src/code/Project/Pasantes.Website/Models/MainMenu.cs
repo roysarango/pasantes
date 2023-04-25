2c87eda2de1dc4879aa71081a84a96a47249032e
roy.sarango <rsa@alpha-solutions.us>
2023-04-24 21:22:13 -0500

    Add menu and banner components

---
+++ src/code/Project/Pasantes.Website/Models/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using Glass.Mapper.Sc.Fields;
+using Pasantes.Website.Models.GlassMapper;
+
+namespace Pasantes.Website.Models
+{
+    public class MainMenu : BaseItem
+    {
+        public virtual string Title { get; set; }
+
+        public virtual Link Link { get; set; }
+    }
+}
---
+++ src/code/Project/Pasantes.Website/Models/MainMenuContainer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using BaseItem = Pasantes.Website.Models.GlassMapper.BaseItem;
+
+namespace Pasantes.Website.Models
+{
+    public class MainMenuContainer : BaseItem
+    {
+        public virtual List<MainMenu> MenuOptions  { get; set; }
+    }
+}
